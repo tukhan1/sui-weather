8906670b24a113de696e73656b5b5548a537fcf9
Egor <tushev.ea@yandex.com>
2024-05-12 18:04:39 +0300

    Initial Commit

---
+++ sui-weather/sui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  sui_weatherApp.swift
+//  sui-weather
+//
+//  Created by Egor Tushev on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct sui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
